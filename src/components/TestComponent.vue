803e6cf725e8ea7e2047fab8af7d6b40cc0c125b
dan koenig <dangerbird@dans-MBP.attlocal.net>
2023-11-22 19:35:17 -0500

    test repp

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>{{ testMessage }}</div>
+    <div v-if="propMessage">{{ propMessage }}</div>
+</template>
+<script>
+export default {
+    props: {
+        propMessage: {
+            required: false,
+            default: '',
+            type: String,
+        }
+    },
+    data () {
+        return {
+            testMessage: 'This is a test'
+        };
+    }
+}
+</script>
